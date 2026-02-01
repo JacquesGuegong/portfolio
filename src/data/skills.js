ec46d222b63c8eae0cc56cd2c4a757953d051191
JacquesGuegong <168654500+JacquesGuegong@users.noreply.github.com>
2026-02-01 13:48:07 -0600

    skills Updated

--- src/data/skills.js
+++ src/data/skills.js
@@ -6,7 +6,7 @@
 
 export const skills = {
   Frontend: ['React', 'JavaScript (ES6+)', 'HTML5', 'CSS3', 'React Native'],
-  Backend: ['Django', 'REST APIs', 'Python'],
+  Backend: ['Django', 'REST APIs', 'Python', 'Java', 'C++', 'C#'],
   Database: ['MySQL', 'Database Design'],
   Tools: ['Git', 'Axios', 'RESTful Services', 'Version Control'],
   Methodologies: ['Agile', 'Scrum', 'TDD (Test-Driven Development)', 'Sprint Planning', 'Trello']
